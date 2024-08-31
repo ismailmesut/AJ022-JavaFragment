f5adf389a419d6cf439634f8af39f649f0457a84
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-31 11:53:41 +0300

    updated: connecting fragments to main activity

--- app/src/main/java/com/ismailmesutmujde/javafragment/FirstFragment.java
+++ app/src/main/java/com/ismailmesutmujde/javafragment/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +16,10 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_first, container, false);
+        TextView textView = viewGroup.findViewById(R.id.textView);
+        textView.setText("1st Fragment Example");
 
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return viewGroup;
     }
 }
--- app/src/main/java/com/ismailmesutmujde/javafragment/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javafragment/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +19,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToFirst(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frame_layout, firstFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout, firstFragment).commit();
 
     }
     public void goToSecond(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SecondFragment secondFragment = new SecondFragment();
+        //fragmentTransaction.add(R.id.frame_layout, secondFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout, secondFragment).commit();
 
     }
 }
